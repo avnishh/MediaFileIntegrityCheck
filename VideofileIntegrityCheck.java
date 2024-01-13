9f44a5c9546d0c818afce8a62d51425af623093f
unknown <kkumar.avnishh@gmail.com>
2024-01-13 22:28:17 +0530

    second

--- VideofileIntegrityCheck.java
+++ VideofileIntegrityCheck.java
@@ -2,16 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class z {
+public class VideofileIntegrityCheck {
 
     public static void main(String[] args) {
         String videoFilePath = "C:\\Users\\Avnish\\Desktop\\ffmpeg\\a.mp4";  // Replace with the actual path to your video file
 
         try {
             // Build the FFmpeg command
-            String[] command = {"ffmpeg", "-v", "error", "-i", videoFilePath, "-f", "null", "-"};
+           String[] command = {"ffmpeg", "-v", "error", "-i", videoFilePath, "-f", "null", "-"};
+          // String command = "ffmpeg -v error -i C:\\Users\\Avnish\\Desktop\\ffmpeg\\a.mp4 -f null -";
 
             // Create a process builder
+          //  ProcessBuilder processBuilder = new ProcessBuilder(command.split("//s+"));
             ProcessBuilder processBuilder = new ProcessBuilder(command);
 
             // Redirect error stream to the input stream
